897f5bd0840508c4fe7c723cae6e4feec257cbd4
Penomatikus <sjagdmann10@googlemail.com>
2015-11-07 23:05:48 +0100

    Finished prototyp model

--- PeachCrawler/Downloader.py
+++ PeachCrawler/Downloader.py
@@ -2,14 +2,10 @@
 from urllib2 import urlopen
 import traceback
 
-# Class definition of the class Downloader
+# Class definition of the class Downloader.
 # Provids an object to download a specific
 # URL.
 class Downloader:
-
-    #Constructor
-    def __init__(self):
-        self.url = "url"
 
     #Std. method. Just for information purpose.
     def __str__(self):
@@ -18,7 +14,6 @@ class Downloader:
     # Method to download a given web-location.
     # Returns the context of the html document as string
     def download(self, webLocation):
-        #print(webLocation)
         html = ''
         try:
             url = urlopen(webLocation)
